3c10ee02b354f4d1f3d51f7624ab7f16e92dd8b4
TonnyMusembi <musembutonnyj@gmail.com>
2022-11-07 13:06:23 +0300

    add

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+
+class FormController extends Controller
+{
+    //
+
+    public function index(){
+        return view ('form');
+
+    }
+    public function store(Request $request){
+
+    }
+}
